5b59e475be34d5341f4ca72afed48292e0bfdc69
Salifu Mutaru <salifumutaru@gmail.com>
2016-08-20 07:33:53 +0000

    Navigating to end of book and start of book work

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,7 +1,34 @@
-function startOfBook(){
-  alert("move to start");
+//url to first page -- to be loaded from disk as attribute of book
+var firstPageUrl = '@public@vhost@g@gutenberg@html@files@52800@52800-h@52800-h-6.htm.html#pgepubid00004';
+//url to last page -- to be loaded from disk as attribute of book
+var lastPageUrl  = '@public@vhost@g@gutenberg@html@files@52800@52800-h@52800-h-57.htm.html#pgepubid01563';
+
+/**
+*
+* startOfBook(Book)
+*
+* Navigates to the start of content of book
+*
+* @param book is an epub Book objct
+* @return Object|bool returns promise object after rendering to page 1 or false on failure
+*/
+function startOfBook(book){
+  book.goto(firstPageUrl);
 }
 
-function endOfBook(){
-  alert("move to end");
+/**
+*
+* endOfBook(Book)
+*
+* Navigates to the end of content of book
+*
+* @param book is an epub Book objct
+* @return Object|bool returns promise object after rendering to last or false on failure
+*/
+function endOfBook(book){
+  book.goto(lastPageUrl, true);
 }
+
+Book.getToc().then(function(toc){
+    console.log(toc);
+});
